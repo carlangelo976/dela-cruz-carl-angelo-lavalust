230ed795942a4b5828a224de2a5bded7898b48a4
carlangelo976 <kyrie6072@gmail.com>
2026-09-05 11:29:56 +0800

    user controller configuration 1

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,22 +1,17 @@
+   <?php
+   defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-<?php
-defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+   class UsersController extends Controller
+   {
+       public function __construct()
+       {
+           parent::__construct();
+           // $this->call->model('UsersModel'); // <-- TEMPORARILY COMMENTED OUT
+       }
 
-class UsersController extends Controller
-{
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->call->model('UsersModel');
-    }
-
-    public function index()
-    {
-        $users = $this->UsersModel->all();
-
-        $this->call->view('users', ['users' => $users]);
-    }
-    //ang model ang nakuha ng database credential mula sa .env file at ang controller ang nag iinvoke ng model para makuha ang data mula sa database at ipasa ito sa view.
-}
+       public function index()
+       {
+           // $users = $this->UsersModel->all(); // <-- TEMPORARILY COMMENTED OUT
+           $this->call->view('users', ['users' => []]); // Pass empty array for now
+       }
+   }
